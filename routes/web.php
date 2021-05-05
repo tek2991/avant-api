be41e7c90f821e2164f9119dfd9af0124727e7ba
tek2991 <eliezer.kt@gmail.com>
2021-05-05 08:01:57 +0530

    Fixed deleted spa routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\SpaLoginController;
+use App\Http\Controllers\Auth\SpaLogoutController;
+use App\Http\Controllers\Auth\SpaRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,5 +24,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::prefix('pwa/')->group(function () {
+    Route::post('register', [SpaRegisterController::class, 'register'])->name('register');
+    Route::post('director-login', [SpaLoginController::class, 'directorLogin'])->name('director-login');
+    Route::post('logout', [SpaLogoutController::class, 'logout'])->name('logout');
+});
 
 require __DIR__.'/auth.php';
