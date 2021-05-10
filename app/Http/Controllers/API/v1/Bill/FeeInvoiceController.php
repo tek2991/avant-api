c52b69995f92cf41f9b8b67adee023394912408f
tek2991 <eliezer.kt@gmail.com>
2021-05-10 07:04:55 +0530

    Created FeeInvoiceController

---
+++ app/Http/Controllers/API/v1/Bill/FeeInvoiceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Bill;
+
+use App\Http\Controllers\Controller;
+use App\Models\FeeInvoice;
+use Illuminate\Http\Request;
+
+class FeeInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FeeInvoice  $feeInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FeeInvoice $feeInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FeeInvoice  $feeInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FeeInvoice $feeInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FeeInvoice  $feeInvoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FeeInvoice $feeInvoice)
+    {
+        //
+    }
+}
