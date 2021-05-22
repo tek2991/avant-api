c848d581d4ed90ae46ae834418833da6517dae2a
tek2991 <eliezer.kt@gmail.com>
2021-05-22 19:12:30 +0530

    Section CRUD complete

--- app/Http/Controllers/API/v1/Setup/SectionController.php
+++ app/Http/Controllers/API/v1/Setup/SectionController.php
@@ -17,7 +17,7 @@ class SectionController extends Controller
      */
     public function index()
     {
-        return Section::paginate();
+        return Section::orderBy('id')->paginate();
     }
 
     /**
