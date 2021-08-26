4f83e4c651412449d2d0986dd0c430595ce803a3
tek2991 <eliezer.kt@gmail.com>
2021-08-26 09:17:32 +0530

    Added caching to Student chart controller methods

--- app/Http/Controllers/API/v1/Chart/StudentChartController.php
+++ app/Http/Controllers/API/v1/Chart/StudentChartController.php
@@ -15,8 +15,12 @@ class StudentChartController extends Controller
     {
         $session = Session::where('is_active', true)->firstOrFail();
 
-        $attendances = $user->attendances()->select('attendance_date', 'attendance_state_id')->where('session_id', $session->id)->get()->groupBy(function ($val) {
-            return Carbon::parse($val->attendance_date)->format('M Y');
+        $key = 'student_dashboard_attendances_' . $user->id;
+
+        $attendances = cache()->remember($key, 60 * 60 * 24, function () use ($user, $session) {
+            return $user->attendances()->select('attendance_date', 'attendance_state_id')->where('session_id', $session->id)->get()->groupBy(function ($val) {
+                return Carbon::parse($val->attendance_date)->format('M Y');
+            });
         });
 
         return $attendances;
@@ -31,18 +35,21 @@ class StudentChartController extends Controller
 
     public function allChaptersInProgress(User $user)
     {
+        $key = 'student_dashboard_allChaptersInProgress_' . $user->id;
 
-        $chapters = $user->student->chapters()->whereHas(
-            'chapterProgressions',
-            function($query) use ($user) {
-                $query->where('section_id', $user->student->sectionStandard->section->id);
-            }
-        )->with([
-            'chapterProgressions' => function($query) use ($user) {
-                $query->where('section_id', $user->student->sectionStandard->section->id);
-            },
-            'subject'
-        ])->get();
+        $chapters = cache()->remember($key, 60 * 60 * 24, function () use ($user) {
+            return $user->student->chapters()->whereHas(
+                'chapterProgressions',
+                function ($query) use ($user) {
+                    $query->where('section_id', $user->student->sectionStandard->section->id);
+                }
+            )->with([
+                'chapterProgressions' => function ($query) use ($user) {
+                    $query->where('section_id', $user->student->sectionStandard->section->id);
+                },
+                'subject'
+            ])->get();
+        });
 
         return $chapters;
     }
