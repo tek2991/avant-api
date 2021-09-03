b6381268e1f8765e9fdb1a0bd68fc61a79716082
eliezer <eliezer.kt@gmail.com>
2021-09-03 12:44:58 +0530

    Student import in progress

--- app/Http/Controllers/API/v1/Import/StudentImportController.php
+++ app/Http/Controllers/API/v1/Import/StudentImportController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\API\v1\Import;
 
 use Illuminate\Http\Request;
+use App\Imports\StudentImport;
 use App\Http\Controllers\Controller;
 
 class StudentImportController extends Controller
 {
     public function store(Request $request){
-        dd($request);
+        $file = $request->file('file')->store('import');
+        Excel::import(new StudentImport, $file);
+        return back()->withStatus('excel file imported');
     }
 }
--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class StudentImport implements ToCollection
+class StudentImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param Collection $collection
