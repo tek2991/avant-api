d2d4389e390cb6f37de60b386db41b3fa28b9024
eliezer <eliezer.kt@gmail.com>
2021-09-04 11:13:53 +0530

    Changed btn style for Upload Holiday List

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -16,7 +16,7 @@
                 Upload Teacher Data
             </a>
             <a href="#"
-                class="w-1/3 h-16 flex items-center justify-center rounded-md border bg-black text-white">
+                class="w-1/3 h-16 flex items-center justify-center rounded-md border border-gray-300">
                 Upload Holiday List
             </a>
         </div>
