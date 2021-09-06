851dd9e11f014328b945d2d88eed5d175bb7686d
tek2991 <eliezer.kt@gmail.com>
2021-09-06 06:40:03 +0530

    Updated VariableSeeder

--- database/seeders/VariableSeeder.php
+++ database/seeders/VariableSeeder.php
@@ -22,6 +22,11 @@ class VariableSeeder extends Seeder
             'RAZORPAY_KEY_SECRET' => "pWxM31ttoWmu7qOCdeKPvqvX",
             'RAZORPAY_CURRENCY' => "INR",
             'RAZORPAY_WEBHOOK_SECRET' => "r4ZVPgiYZYJdYRKSm4TRNEbIT40J4g8g",
+            'ADDRESS_LINE_1' => "Avant SMS",
+            'ADDRESS_LINE_2' => "Webrefiner Pvt Ltd",
+            'ADDRESS_LINE_3' => "Guwahati, Assam, India 781001",
+            'GST' => "18HAGTS5485RT",
+            'LOGO' => "https://resources.ap-south-1.linodeobjects.com/logo.png"
         );
 
         foreach ($variables as $key => $value) {
