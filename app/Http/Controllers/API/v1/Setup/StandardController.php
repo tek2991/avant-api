126f21b1e73e4aae558e86725017e48180275df1
tek2991 <eliezer.kt@gmail.com>
2021-09-07 07:55:24 +0530

    Added phone to response

--- app/Http/Controllers/API/v1/Setup/StandardController.php
+++ app/Http/Controllers/API/v1/Setup/StandardController.php
@@ -32,7 +32,7 @@ class StandardController extends Controller
 
     public function studentByStandard(Standard $standard){
         return $standard->students()->with([
-            'user:id','user.userDetail:user_id,name', 'sectionStandard.standard:id,name', 'sectionStandard.section:id,name'
+            'user:id','user.userDetail:user_id,name,phone', 'sectionStandard.standard:id,name', 'sectionStandard.section:id,name'
         ])->orderBy('section_standard_id')->orderBy('roll_no')->get();
     }
 
