82d70a2531db17e4a40c245490432faf81124029
tek2991 <eliezer.kt@gmail.com>
2021-09-11 08:34:49 +0530

    Sample SemdSms Job added

--- app/Jobs/SendSmsJob.php
+++ app/Jobs/SendSmsJob.php
@@ -13,14 +13,18 @@ class SendSmsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $variables;
+    public $numbers;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($variables, $numbers)
     {
-        //
+        $this->variables = $variables;
+        $this->numbers = $numbers;
     }
 
     /**
@@ -30,6 +34,44 @@ class SendSmsJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $fields = [
+            "sender_id" => "DLT_SENDER_ID",
+            "message" => "YOUR_MESSAGE_ID",
+            "variables_values" => "12345|asdaswdx",
+            "route" => "dlt",
+            "numbers" => "9999999999,8888888888,7777777777",
+        ];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://www.fast2sms.com/dev/bulkV2",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($fields),
+            CURLOPT_HTTPHEADER => array(
+                "authorization: YOUR_API_KEY",
+                "accept: */*",
+                "cache-control: no-cache",
+                "content-type: application/json"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
     }
 }
