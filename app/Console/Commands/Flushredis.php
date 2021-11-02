bb16d9a4084de27b983a1f68022579346f193172
tek2991 <eliezer.kt@gmail.com>
2021-11-02 07:30:38 +0530

    Added description for redis flush command

--- app/Console/Commands/Flushredis.php
+++ app/Console/Commands/Flushredis.php
@@ -19,7 +19,7 @@ class Flushredis extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Flush Redis Database';
 
     /**
      * Create a new command instance.
