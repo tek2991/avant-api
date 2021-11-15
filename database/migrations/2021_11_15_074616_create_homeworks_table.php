a3dec094a1f6c4490aaada94e8a030bd604e9eac
tek2991 <eliezer.kt@gmail.com>
2021-11-15 08:54:29 +0530

    Added migrations for homeworks

---
+++ database/migrations/2021_11_15_074616_create_homeworks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homeworks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('session_id')->constrained();
+            $table->foreignId('section_standard_id')->constrained('section_standard');
+            $table->foreignId('subject_id')->constrained();
+            $table->foreignId('chapter_id')->nullable()->constrained();
+            $table->string('name');
+            $table->string('description');
+            $table->foreignId('created_by')->constrained('users', 'id');
+            $table->date('homework_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('homeworks');
+    }
+}
