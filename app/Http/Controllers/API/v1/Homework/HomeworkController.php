63197f12dc151e9a9c5bf6e3535785acc4da1b2a
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-11-16 11:18:19 +0530

    Added Homework Controller

---
+++ app/Http/Controllers/API/v1/Homework/HomeworkController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Homework;
+
+use App\Http\Controllers\Controller;
+use App\Models\Homework;
+use Illuminate\Http\Request;
+
+class HomeworkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Homework $homework)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Homework $homework)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Homework  $homework
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Homework $homework)
+    {
+        //
+    }
+}
