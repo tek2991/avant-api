12f4f51c408ec2a815cbc11f86672e469db7d66e
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-11-22 15:16:31 +0530

    Created notifications migrations

---
+++ database/migrations/2021_11_22_151233_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->foreignId('created_by')->constrained('users', 'id');
+            $table->foreignId('notification_type_id')->constrained('notification_types', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
