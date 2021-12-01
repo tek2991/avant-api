c65398788416873c1e16b50b20b59b24d8d28ee0
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2021-12-01 14:53:23 +0530

    Updated exams migrations and added exam section_standard pivot

--- database/migrations/2021_09_23_072928_create_exams_table.php
+++ database/migrations/2021_09_23_072928_create_exams_table.php
@@ -19,6 +19,8 @@ class CreateExamsTable extends Migration
             $table->string('name');
             $table->string('description')->nullable();
             $table->foreignId('exam_type_id')->constrained('exam_types');
+            $table->dateTime('exam_start_date');
+            $table->dateTime('exam_end_date');
             $table->foreignId('created_by')->constrained('users', 'id');
             $table->timestamps();
         });
