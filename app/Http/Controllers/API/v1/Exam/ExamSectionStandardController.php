61e7214a1b53fe2ef440da88643792924421061b
tek2991 <eliezer.kt@gmail.com>
2021-12-20 07:08:17 +0530

    Created ExamSectionStandardController

---
+++ app/Http/Controllers/API/v1/Exam/ExamSectionStandardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Exam;
+
+use App\Http\Controllers\Controller;
+use App\Models\ExamSectionStandard;
+use Illuminate\Http\Request;
+
+class ExamSectionStandardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $examSectionStandard = ExamSectionStandard::orderBy('id', 'desc')->paginate();
+        return $examSectionStandard;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ExamSectionStandard  $examSectionStandard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExamSectionStandard $examSectionStandard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ExamSectionStandard  $examSectionStandard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExamSectionStandard $examSectionStandard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ExamSectionStandard  $examSectionStandard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExamSectionStandard $examSectionStandard)
+    {
+        //
+    }
+}
