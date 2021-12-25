af3a060917f04120282b6405e1bc96f4335dbca0
tek2991 <eliezer.kt@gmail.com>
2021-12-25 13:32:44 +0530

    updated tinymce-config file

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
+<script>
+  tinymce.init({
+    selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+    plugins: 'code table lists',
+    toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+  });
+</script>
