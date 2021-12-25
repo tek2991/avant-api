5b2ce1d800402139ffc51ba3fbe4f2bef0fb1418
tek2991 <eliezer.kt@gmail.com>
2021-12-25 13:36:02 +0530

    Basic Tine-mce demo added

--- resources/views/tech-demos/tiny-mce.blade.php
+++ resources/views/tech-demos/tiny-mce.blade.php
@@ -4,8 +4,10 @@
             {{ __('Tiny MCE Demo') }}
         </h2>
     </x-slot>
-
+    
     <div class="py-12">
         <h5>Tiny MCE Demo</h5>
+        <x-head.tinymce-config />
+        <x-forms.tinymce-editor />
     </div>
 </x-app-layout>
