0599453d9afaf25a0a574055bfc0dc3ee75e8e38
tek2991 <eliezer.kt@gmail.com>
2022-02-19 11:11:34 +0530

    Updated ExamUserController

--- app/Http/Controllers/API/v1/Exam/ExamUserController.php
+++ app/Http/Controllers/API/v1/Exam/ExamUserController.php
@@ -15,45 +15,46 @@ class ExamUserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Exam $exam)
+    public function index(Exam $exam, Request $request)
     {
         $user = Auth::user();
-        if ($user->hasRole('director') !== true && $user->hasRole('teacher') !== true) {
+        if ($user->hasRole('director') !== true && $user->hasRole('teacher') !== true && $request->filled('user_id') !== true) {
             return response([
                 'header' => 'Forbidden',
                 'message' => 'Please Logout and Login again.'
             ], 401);
         }
 
-        $this->validate(request(), [
+        $this->validate($request, [
             'standard_id' => 'bail|nullable|integer|exists:standards,id',
             'section_id' => 'bail|nullable|integer|exists:sections,id',
+            'user_id' => 'bail|nullable|integer|exists:users,id',
         ]);
 
-        $exam_users = ExamUser::where('exam_id', $exam->id)
-        ->whereHas('user', function ($query) {
-            $query->whereHas('student', function ($query) {
-                $query->whereHas('sectionStandard', function ($query) {
-                    $query->whereHas('standard', function ($query) {
-                        if(request()->filled('standard_id')) {
-                            $query->where('id', request()->standard_id);
-                        }
-                        $query->orderBy('id');
+        $query = ExamUser::where('exam_id', $exam->id)
+            ->whereHas('user', function ($query) {
+                $query->whereHas('student', function ($query) {
+                    $query->whereHas('sectionStandard', function ($query) {
+                        $query->whereHas('standard', function ($query) {
+                            if (request()->filled('standard_id')) {
+                                $query->where('id', request()->standard_id);
+                            }
+                            $query->orderBy('id');
+                        });
+                        $query->whereHas('section', function ($query) {
+                            if (request()->filled('section_id')) {
+                                $query->where('id', request()->section_id);
+                            }
+                            $query->orderBy('id');
+                        });
+                        $query->orderBy('roll_no');
                     });
-
-                    $query->whereHas('section', function ($query) {
-                        if(request()->filled('section_id')) {
-                            $query->where('id', request()->section_id);
-                        }
-                        $query->orderBy('id');
-                    });
-
-                    $query->orderBy('roll_no');
                 });
             });
-        })
-        ->with('user.userDetail', 'user.student.sectionStandard.section', 'user.student.sectionStandard.standard')->paginate();
-
+        if (request()->filled('user_id')) {
+            $query->where('user_id', request()->user_id);
+        }
+        $exam_users = $query->with('user.userDetail', 'user.student.sectionStandard.section', 'user.student.sectionStandard.standard')->paginate();
         return $exam_users;
     }
 
@@ -73,16 +74,13 @@ class ExamUserController extends Controller
                 'message' => 'Please Logout and Login again.'
             ], 401);
         }
-
         $this->validate($request, [
             'exam_user_state_id' => 'bail|required|integer|exists:exam_user_states,id',
         ]);
-
         $examUser->update([
             'exam_user_state_id' => $request->exam_user_state_id,
         ]);
-
-        return response ([
+        return response([
             'header' => 'Success',
             'message' => 'Exam User State Updated Successfully.'
         ], 200);
