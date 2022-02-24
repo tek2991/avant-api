ea7a4377738e7f2517b1a8517a17cf48827aeabb
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-24 15:07:40 +0530

    Updated Admit Card

--- resources/views/documents/admit-card.blade.php
+++ resources/views/documents/admit-card.blade.php
@@ -63,9 +63,9 @@
     <div class="a4-container">
         <table style="border: none">
             <tr>
-                <th>
+                <th style="text-align: left">
                     {{-- <img src="{{ url('/img/board_logo.png') }}" alt="seba_logo" class="logo"> --}}
-                    <img src="{{ public_path('img/board_logo.png') }}" alt="board_logo" class="r_logo">
+                    <img src="{{ public_path('img/board_logo.png') }}" alt="board_logo" class="logo">
                 </th>
                 <th class="school-heading">
                     <span style="font-size: 1.5rem;">{{ $variables['ADDRESS_LINE_1'] }}</span> <br>
@@ -73,9 +73,9 @@
                     {{ $variables['ADDRESS_LINE_3'] }}<br>
                     School Reg. ID: {{ $variables['SCHOOL_REG_ID'] }}
                 </th>
-                <th>
+                <th style="text-align: right">
                     {{-- <img src="{{ url('/img/school_logo.png') }}" alt="school_logo" class="logo"> --}}
-                    <img src="{{ public_path('img/school_logo.png') }}" alt="school_logo" class="r_logo">
+                    <img src="{{ public_path('img/school_logo.png') }}" alt="school_logo" class="logo">
                 </th>
             </tr>
         </table>
@@ -136,7 +136,7 @@
                     {{-- <img src="{{ $exam_user->user->profilePicture->url ? url('storage/' . $exam_user->user->profilePicture->url) : url('/img/profile.png') }}"
                         alt="seba_logo" style="max-width: 150px"> --}}
                     <img src="{{ $exam_user->user->profilePicture->url ? public_path('storage/' . $exam_user->user->profilePicture->url) : public_path('img/logo.png') }}"
-                        style="width: 100%; max-width: 150px" />
+                        style="max-width: 150px; max-height: 200px" />
                 </td>
             </tr>
         </table>
