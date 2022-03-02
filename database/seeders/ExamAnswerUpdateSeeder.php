ad8fbc11fd28ed4aeca3bd078a1158250a47c397
tek2991 <eliezer.kt@gmail.com>
2022-03-03 05:13:16 +0530

    Updated Seeders

--- database/seeders/ExamAnswerUpdateSeeder.php
+++ database/seeders/ExamAnswerUpdateSeeder.php
@@ -20,37 +20,28 @@ class ExamAnswerUpdateSeeder extends Seeder
      */
     public function run()
     {
-        ExamAnswer::chunk(100, function($answers){
+        ExamAnswer::chunk(100, function ($answers) {
             foreach ($answers as $answer) {
                 $question_marks = $answer->examQuestion->marks;
                 $exam_subject_id = $answer->examQuestion->examSubject->id;
                 $user_id = $answer->user->id;
-                $exam_subject_score = ExamSubjectScore::where('exam_subject_id', $exam_subject_id)->where('user_id', $user_id)->first();
-                $marks_secured = $exam_subject_score->marks_secured;
                 if ($answer->examQuestion->exam_question_type_id == 2) {
-                    // $marks = rand(1, $question_marks);
+                    $marks = rand(1, $question_marks);
                     $answer->update([
-                        'exam_answer_state_id' => 2,
-                        'description' => $this->faker->sentence,
-                        // 'marks_secured' => $marks,
+                        // 'exam_answer_state_id' => 2,
+                        // 'description' => $this->faker->sentence,
+                        'marks_secured' => $marks,
+                        'evaluated_by' => 2,
                     ]);
-
-                    // $exam_subject_score->update([
-                    //     'marks_secured' => $marks_secured + $marks,
-                    // ]);
                 } else {
-                    $exam_question_options = $answer->examQuestion->examQuestionOptions()->get();
-                    $random_option = $exam_question_options->random();
-                    $marks = $random_option->is_correct ? $question_marks : 0;
-                    $answer->update([
-                        'exam_answer_state_id' => 2,
-                        'exam_question_option_id' => $random_option->id,
-                        'marks_secured' => $marks
-                    ]);
-
-                    $exam_subject_score->update([
-                        'marks_secured' => $marks_secured + $marks,
-                    ]);
+                    // $exam_question_options = $answer->examQuestion->examQuestionOptions()->get();
+                    // $random_option = $exam_question_options->random();
+                    // $marks = $random_option->is_correct ? $question_marks : 0;
+                    // $answer->update([
+                    //     'exam_answer_state_id' => 2,
+                    //     'exam_question_option_id' => $random_option->id,
+                    //     'marks_secured' => $marks
+                    // ]);
                 }
             }
         });
---
+++ database/seeders/ExamScoreUpdateSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ExamAnswer;
+use App\Models\ExamQuestion;
+use Illuminate\Database\Seeder;
+use App\Models\ExamSubjectScore;
+
+class ExamScoreUpdateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $exam_subject_scores = ExamSubjectScore::where('exam_subject_state_id', 2)->get();
+        foreach($exam_subject_scores as $score){
+            $exam_question_ids = ExamQuestion::where('exam_subject_id', $score->exam_subject_id)->pluck('id');
+            $total_marks_secured = ExamAnswer::where('user_id', $score->user_id)->whereIn('exam_question_id', $exam_question_ids)->sum('marks_secured');
+            $score->update([
+                'marks_secured' => $total_marks_secured,
+                'evaluated_by' => 2,
+            ]);
+        }
+    }
+}
