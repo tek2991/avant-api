75ea04536ec43ba9da78371ec74b65718b468ab2
tek2991 <eliezer.kt@gmail.com>
2022-03-03 05:13:47 +0530

    Updated seeder

--- database/seeders/ExamAnswerUpdateSeeder.php
+++ database/seeders/ExamAnswerUpdateSeeder.php
@@ -28,20 +28,20 @@ class ExamAnswerUpdateSeeder extends Seeder
                 if ($answer->examQuestion->exam_question_type_id == 2) {
                     $marks = rand(1, $question_marks);
                     $answer->update([
-                        // 'exam_answer_state_id' => 2,
-                        // 'description' => $this->faker->sentence,
+                        'exam_answer_state_id' => 2,
+                        'description' => $this->faker->sentence,
                         'marks_secured' => $marks,
                         'evaluated_by' => 2,
                     ]);
                 } else {
-                    // $exam_question_options = $answer->examQuestion->examQuestionOptions()->get();
-                    // $random_option = $exam_question_options->random();
-                    // $marks = $random_option->is_correct ? $question_marks : 0;
-                    // $answer->update([
-                    //     'exam_answer_state_id' => 2,
-                    //     'exam_question_option_id' => $random_option->id,
-                    //     'marks_secured' => $marks
-                    // ]);
+                    $exam_question_options = $answer->examQuestion->examQuestionOptions()->get();
+                    $random_option = $exam_question_options->random();
+                    $marks = $random_option->is_correct ? $question_marks : 0;
+                    $answer->update([
+                        'exam_answer_state_id' => 2,
+                        'exam_question_option_id' => $random_option->id,
+                        'marks_secured' => $marks
+                    ]);
                 }
             }
         });
