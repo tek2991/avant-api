d95fd0218ac7fcce315decf65246c736e50e391d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-05-02 00:11:16 +0530

    Updated dompef settings

--- config/dompdf.php
+++ config/dompdf.php
@@ -231,7 +231,7 @@ return array(
          *
          * @var bool
          */
-        "enable_remote" => true,
+        "enable_remote" => false,
 
         /**
          * A ratio applied to the fonts height to be more like browsers' line height
